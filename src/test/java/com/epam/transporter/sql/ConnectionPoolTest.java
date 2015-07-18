4f096f0fe39b444c2b9255161b6cc1cc17151dc9
zateyev <zhasulan.ateyev@gmail.com>
2015-07-18 22:04:05 +0600

    adding JUnit

---
+++ src/test/java/com/epam/transporter/sql/ConnectionPoolTest.java
@@ -0,0 +1,33 @@
+package com.epam.transporter.sql;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConnectionPoolTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGetInstance() throws Exception {
+    }
+
+    @Test
+    public void testGetConnection() throws Exception {
+    }
+
+    @Test
+    public void testFreeConnection() throws Exception {
+    }
+
+    @Test
+    public void testRelease() throws Exception {
+    }
+}
