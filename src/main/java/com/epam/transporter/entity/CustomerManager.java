1ecbf79df870c443aeffb3f200e2fa1cc587de2f
zateyev <zhasulan.ateyev@gmail.com>
2015-07-19 19:29:54 +0600

    using HikariCP

--- src/main/java/com/epam/transporter/entity/CustomerManager.java
+++ src/main/java/com/epam/transporter/entity/CustomerManager.java
@@ -1,15 +1,33 @@
 package com.epam.transporter.entity;
 
-import java.sql.*;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class CustomerManager {
     public Customer findCustomerByEmail(String email) {
         Customer customer = new Customer();
         customer.setEmail(email);
         try {
-            Class.forName("org.h2.Driver");
-            String url = "jdbc:h2:tcp://localhost/~/transporter";
-            Connection connection = DriverManager.getConnection(url, "transporter", "transporter");
+            /*adding properties to HikariCP using property-file
+            Properties properties = new Properties();
+            InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("db/hikari.properties");
+            try {
+                properties.load(resourceAsStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }*/
+            HikariConfig hikariConfig = new HikariConfig();
+            hikariConfig.setJdbcUrl("jdbc:h2:tcp://localhost/~/transporter");
+            hikariConfig.setUsername("transporter");
+            hikariConfig.setPassword("transporter");
+            HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+            Connection connection = dataSource.getConnection();
+
             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM CUSTOMER WHERE EMAIL = ?");
             preparedStatement.setString(1, email);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -19,7 +37,7 @@ public class CustomerManager {
                 customer.setPassword(password);
                 customer.setFirstName(firstName);
             }
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return customer;
