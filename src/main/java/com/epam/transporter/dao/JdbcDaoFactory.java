d043cfe85db5a33cf8fcd758461ae409d59cd5a2
zateyev <zhasulan.ateyev@gmail.com>
2015-08-10 16:45:26 +0600

    adding property file for h2 DB settings

--- src/main/java/com/epam/transporter/dao/JdbcDaoFactory.java
+++ src/main/java/com/epam/transporter/dao/JdbcDaoFactory.java
@@ -3,14 +3,16 @@ package com.epam.transporter.dao;
 import com.epam.transporter.sql.ConnectionPool;
 
 import java.sql.Connection;
+import java.util.ResourceBundle;
 
 public class JdbcDaoFactory extends DaoFactory {
-    public static final String DRIVER_NAME = "org.h2.Driver";
-    public static final String URL = "jdbc:h2:tcp://localhost/~/transporter";
-    public static final String USER = "transporter";
-    public static final String PASSWORD = "transporter";
-    public static final int MAX_CONNECTION = 5;
-    private static ConnectionPool connectionPool = ConnectionPool.getInstance(DRIVER_NAME, URL, USER, PASSWORD, MAX_CONNECTION);
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle("h2Database");
+    private static String driverName = resourceBundle.getString("db.driver");
+    private static String url = resourceBundle.getString("db.url");
+    private static String user = resourceBundle.getString("db.user");
+    private static String password = resourceBundle.getString("db.password");
+    private static Integer maxConnection = Integer.valueOf(resourceBundle.getString("db.maxconnection"));
+    private static ConnectionPool connectionPool = ConnectionPool.getInstance(driverName, url, user, password, maxConnection);
 
     public static Connection createConnection() {
         return connectionPool.getConnection();
